b01c30a6e1e07162ec1c9270b2990c17fe7caded
ujjvalksharma <ujjvalksharma@gmail.com>
2022-05-14 15:00:08 -0700

    Implemented about me button to display email and name

---
+++ app/src/main/java/com/example/numad_ujjvalsharma/CustomWidget/Ibutton.java
@@ -0,0 +1,8 @@
+package com.example.numad_ujjvalsharma.CustomWidget;
+
+import android.view.View;
+
+public interface Ibutton {
+
+    public void onClick(View view);
+}
--- app/src/main/java/com/example/numad_ujjvalsharma/MainActivity.java
+++ app/src/main/java/com/example/numad_ujjvalsharma/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.numad_ujjvalsharma;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.numad_ujjvalsharma.CustomWidget.Ibutton;
+
+
+public class MainActivity extends AppCompatActivity implements Ibutton {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Toast.makeText(this,"Name: Ujjval Sharma and Email: Ujjvalksharma@gmail.com",Toast.LENGTH_LONG).show();
+
     }
 }
